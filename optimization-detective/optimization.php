3d964d7e8bde28412afc7584ed1cb6ab48a41ff6
Sean Murphy <sean@iamseanmurphy.com>
2025-03-17 19:17:11 +0000

    1.0.0-beta3

--- optimization-detective/optimization.php
+++ optimization-detective/optimization.php
@@ -238,91 +238,28 @@ function od_optimize_template_output_buffer( string $buffer ): string {
 	// If the initial tag is not an open HTML tag, then abort since the buffer is not a complete HTML document.
 	$processor = new OD_HTML_Tag_Processor( $buffer );
 	if ( ! (
-		$processor->next_tag() &&
+		$processor->next_tag( array( 'tag_closers' => 'visit' ) ) &&
 		! $processor->is_tag_closer() &&
 		'HTML' === $processor->get_tag()
 	) ) {
 		return $buffer;
 	}
 
-	$slug = od_get_url_metrics_slug( od_get_normalized_query_vars() );
-	$post = OD_URL_Metrics_Post_Type::get_post( $slug );
+	$query_vars = od_get_normalized_query_vars();
+	$slug       = od_get_url_metrics_slug( $query_vars );
+	$post       = OD_URL_Metrics_Post_Type::get_post( $slug );
+
+	/**
+	 * Post ID.
+	 *
+	 * @var positive-int|null $post_id
+	 */
+	$post_id = $post instanceof WP_Post ? $post->ID : null;
 
 	$tag_visitor_registry = new OD_Tag_Visitor_Registry();
 
 	/**
 	 * Fires to register tag visitors before walking over the document to perform optimizations.
-	 *
-	 * Once a page has finished rendering and the output buffer is processed, the page contents are loaded into
-	 * an HTML Tag Processor instance. It then iterates over each tag in the document, and at each open tag it will
-	 * invoke all registered tag visitors. A tag visitor is simply a callable (such as a regular function, closure,
-	 * or even a class with an `__invoke` method defined). The tag visitor callback is invoked by passing an instance
-	 * of the `OD_Tag_Visitor_Context` object which includes the following read-only properties:
-	 *
-	 * - `$processor` (`OD_HTML_Tag_Processor`): The processor with the cursor at the current open tag.
-	 * - `$url_metric_group_collection` (`OD_URL_Metric_Group_Collection`): The URL Metrics which may include information about the current tag to inform what optimizations the callback performs.
-	 * - `$link_collection` (`OD_Link_Collection`): Collection of links which will be added to the `HEAD` when the page is served. This allows you to add preload links and preconnect links as needed.
-	 * - `$url_metrics_id` (`positive-int|null`): The post ID for the `od_url_metrics` post from which the URL Metrics were loaded (if any). For advanced usage.
-	 *
-	 * Note that you are free to call `$processor->next_tag()` in the callback (such as to walk over any child elements)
-	 * since the tag processor's cursor will be reset to the tag after the callback finishes.
-	 *
-	 * When a tag visitor sees it is at a relevant open tag (e.g. by checking `$processor->get_tag()`), it can call the
-	 * `$context->track_tag()` method to indicate that the tag should be measured during detection. This will cause the
-	 * tag to be included among the `elements` in the stored URL Metrics. The element data includes properties such
-	 * as `intersectionRatio`, `intersectionRect`, and `boundingClientRect` (provided by an `IntersectionObserver`) as
-	 * well as whether the tag is the LCP element (`isLCP`) or LCP element candidate (`isLCPCandidate`). This method
-	 * should not be called if the current tag is not relevant for the tag visitor or if the tag visitor callback does
-	 * not need to query the provided `OD_URL_Metric_Group_Collection` instance to apply the desired optimizations. (In
-	 * addition to calling the `$context->track_tag()`, a callback may also return `true` to indicate the tag should be
-	 * tracked.)
-	 *
-	 * Here's an example tag visitor that depends on URL Metrics data:
-	 *
-	 *     $tag_visitor_registry->register(
-	 *         'lcp-img-fetchpriority-high',
-	 *         static function ( OD_Tag_Visitor_Context $context ): void {
-	 *             if ( $context->processor->get_tag() !== 'IMG' ) {
-	 *                 return; // Tag is not relevant for this tag visitor.
-	 *             }
-	 *
-	 *             // Mark the tag for measurement during detection so it is included among the elements stored in URL Metrics.
-	 *             $context->track_tag();
-	 *
-	 *             // Make sure fetchpriority=high is added to LCP IMG elements based on the captured URL Metrics.
-	 *             $common_lcp_element = $context->url_metric_group_collection->get_common_lcp_element();
-	 *             if (
-	 *                 null !== $common_lcp_element
-	 *                 &&
-	 *                 $common_lcp_element->get_xpath() === $context->processor->get_xpath()
-	 *             ) {
-	 *                 $context->processor->set_attribute( 'fetchpriority', 'high' );
-	 *             }
-	 *         }
-	 *     );
-	 *
-	 * Please note this implementation of setting `fetchpriority=high` on the LCP `IMG` element is simplified. Please
-	 * see the Image Prioritizer extension for a more robust implementation.
-	 *
-	 * Here's an example tag visitor that does not depend on any URL Metrics data:
-	 *
-	 *     $tag_visitor_registry->register(
-	 *         'img-decoding-async',
-	 *         static function ( OD_Tag_Visitor_Context $context ): bool {
-	 *             if ( $context->processor->get_tag() !== 'IMG' ) {
-	 *                 return; // Tag is not relevant for this tag visitor.
-	 *             }
-	 *
-	 *             // Set the decoding attribute if it is absent.
-	 *             if ( null === $context->processor->get_attribute( 'decoding' ) ) {
-	 *                 $context->processor->set_attribute( 'decoding', 'async' );
-	 *             }
-	 *         }
-	 *     );
-	 *
-	 * Refer to [Image Prioritizer](https://github.com/WordPress/performance/tree/trunk/plugins/image-prioritizer) and
-	 * [Embed Optimizer](https://github.com/WordPress/performance/tree/trunk/plugins/embed-optimizer) for additional
-	 * examples of how tag visitors are used.
 	 *
 	 * @since 0.3.0
 	 *
@@ -330,22 +267,42 @@ function od_optimize_template_output_buffer( string $buffer ): string {
 	 */
 	do_action( 'od_register_tag_visitors', $tag_visitor_registry );
 
-	$current_etag         = od_get_current_url_metrics_etag( $tag_visitor_registry, $wp_the_query, od_get_current_theme_template() );
-	$group_collection     = new OD_URL_Metric_Group_Collection(
+	$current_etag     = od_get_current_url_metrics_etag( $tag_visitor_registry, $wp_the_query, od_get_current_theme_template() );
+	$group_collection = new OD_URL_Metric_Group_Collection(
 		$post instanceof WP_Post ? OD_URL_Metrics_Post_Type::get_url_metrics_from_post( $post ) : array(),
 		$current_etag,
 		od_get_breakpoint_max_widths(),
 		od_get_url_metrics_breakpoint_sample_size(),
 		od_get_url_metric_freshness_ttl()
 	);
-	$link_collection      = new OD_Link_Collection();
+	$link_collection  = new OD_Link_Collection();
+
+	$template_optimization_context = new OD_Template_Optimization_Context(
+		$group_collection,
+		$link_collection,
+		$query_vars,
+		$slug,
+		$post_id
+	);
+
+	/**
+	 * Fires before Optimization Detective starts iterating over the document in the output buffer.
+	 *
+	 * This is before any of the registered tag visitors have been invoked.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param OD_Template_Optimization_Context $template_optimization_context Template optimization context.
+	 */
+	do_action( 'od_start_template_optimization', $template_optimization_context );
+
 	$visited_tag_state    = new OD_Visited_Tag_State();
 	$tag_visitor_context  = new OD_Tag_Visitor_Context(
 		$processor,
 		$group_collection,
 		$link_collection,
 		$visited_tag_state,
-		$post instanceof WP_Post && $post->ID > 0 ? $post->ID : null
+		$post_id
 	);
 	$current_tag_bookmark = 'optimization_detective_current_tag';
 	$visitors             = iterator_to_array( $tag_visitor_registry );
@@ -355,7 +312,12 @@ function od_optimize_template_output_buffer( string $buffer ): string {
 
 	do {
 		// Never process anything inside NOSCRIPT since it will never show up in the DOM when scripting is enabled, and thus it can never be detected nor measured.
-		if ( in_array( 'NOSCRIPT', $processor->get_breadcrumbs(), true ) ) {
+		// Similarly, elements in the Admin Bar are not relevant for optimization, so this loop ensures that no tags in the Admin Bar are visited.
+		if (
+			in_array( 'NOSCRIPT', $processor->get_breadcrumbs(), true )
+			||
+			$processor->is_admin_bar()
+		) {
 			continue;
 		}
 
@@ -382,27 +344,37 @@ function od_optimize_template_output_buffer( string $buffer ): string {
 		}
 
 		if ( $tracked_in_url_metrics && $needs_detection ) {
-			// TODO: Replace get_stored_xpath with get_xpath once the transitional period is over.
-			$xpath = $processor->get_stored_xpath();
-			$processor->set_meta_attribute( 'xpath', $xpath );
+			$processor->set_meta_attribute( 'xpath', $processor->get_xpath() );
 		}
 
 		$visited_tag_state->reset();
-	} while ( $processor->next_open_tag() );
+	} while ( $processor->next_tag( array( 'tag_closers' => 'skip' ) ) );
+
+	// Inject detection script.
+	// TODO: When optimizing above, if we find that there is a stored LCP element but it fails to match, it should perhaps set $needs_detection to true and send the request with an override nonce. However, this would require backtracking and adding the data-od-xpath attributes.
+	if ( $needs_detection ) {
+		$processor->append_body_html( od_get_detection_script( $slug, $group_collection ) );
+	}
+
+	/**
+	 * Fires after Optimization Detective has finished iterating over the document in the output buffer.
+	 *
+	 * This is after all the registered tag visitors have been invoked.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param OD_Template_Optimization_Context $template_optimization_context Template optimization context.
+	 */
+	do_action( 'od_finish_template_optimization', $template_optimization_context );
 
 	// Send any preload links in a Link response header and in a LINK tag injected at the end of the HEAD.
+	// Additional links may have been added at the od_finish_template_optimization action, so this must come after.
 	if ( count( $link_collection ) > 0 ) {
 		$response_header_links = $link_collection->get_response_header();
 		if ( ! is_null( $response_header_links ) && ! headers_sent() ) {
 			header( $response_header_links, false );
 		}
 		$processor->append_head_html( $link_collection->get_html() );
-	}
-
-	// Inject detection script.
-	// TODO: When optimizing above, if we find that there is a stored LCP element but it fails to match, it should perhaps set $needs_detection to true and send the request with an override nonce. However, this would require backtracking and adding the data-od-xpath attributes.
-	if ( $needs_detection ) {
-		$processor->append_body_html( od_get_detection_script( $slug, $group_collection ) );
 	}
 
 	return $processor->get_updated_html();
